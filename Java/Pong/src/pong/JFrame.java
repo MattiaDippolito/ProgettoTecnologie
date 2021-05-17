7f7d71a10fb00708db8e72c0e49608e21e64f644
danieleoricchio <lele13.do@gmail.com>
2021-05-17 23:00:57 +0200

    Update del frame
    Ho aggiornato il JFrame, aggiungendo il disegno della palla e i punteggi.

--- Java/Pong/src/pong/JFrame.java
+++ Java/Pong/src/pong/JFrame.java
@@ -23,9 +23,9 @@ import java.util.logging.Logger;
 public class JFrame extends javax.swing.JFrame {
 
     ArrayList<Giocatore> Giocatori;
-    //Palla p; da aggiungere
+    Palla p;
     Giocatore g1, g2;
-    //Condivisa c;da aggiungere
+    Condivisa c;
 
     public JFrame() {
         initComponents();
@@ -37,13 +37,13 @@ public class JFrame extends javax.swing.JFrame {
         Giocatori.add(g1);
         g2 = new Giocatore("destra", this);
         Giocatori.add(g2);
-        /*c = Condivisa.getIstance(g1, g2);
-        p = new Palla(this);                  da aggiungere
-        p.start();*/
+        c = Condivisa.getIstance(g1, g2);
+        p = new Palla(this);
+        p.start();
         this.addKeyListener(new keyEvent(g1));
         this.addKeyListener(new KeyEvent2(g2));
-        //Disegna disegna = new Disegna(this, 33);   da aggiungere
-        //disegna.start();
+        Disegna disegna = new Disegna(this, 33);
+        disegna.start();
         this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
     }
 
@@ -52,24 +52,24 @@ public class JFrame extends javax.swing.JFrame {
         if (!g1.pronto || !g2.pronto) {
             Image offscreen3 = createImage(this.getWidth(), this.getHeight());
             Graphics offgc3 = offscreen3.getGraphics();
-            
+
             offgc3.setColor(Color.black);
             offgc3.fillRect(0, 0, this.getWidth(), this.getHeight());
             offgc3.setColor(Color.white);
             Font finizio1 = new Font("Arial", Font.PLAIN, 100);
             offgc3.setFont(finizio1);
-            offgc3.drawString("PONG!", this.getWidth() / 2-130, this.getHeight() - 450);
+            offgc3.drawString("PONG!", this.getWidth() / 2 - 130, this.getHeight() - 450);
             Font finizio2 = new Font("Arial", Font.PLAIN, 30);
             offgc3.setFont(finizio2);
-            if(!g1.pronto){
-                offgc3.drawString("Premere z per G1 Pronto", this.getWidth() / 2-400, this.getHeight() - 300);
-            }else{
-                offgc3.drawString("G1 Pronto", this.getWidth() / 2-400, this.getHeight() - 300);
+            if (!g1.pronto) {
+                offgc3.drawString("Premere z per G1 Pronto", this.getWidth() / 2 - 400, this.getHeight() - 300);
+            } else {
+                offgc3.drawString("G1 Pronto", this.getWidth() / 2 - 400, this.getHeight() - 300);
             }
-            if(!g2.pronto){
-                offgc3.drawString("Premere m per G2 Pronto", this.getWidth() / 2+130, this.getHeight() - 300);
-            }else{
-                offgc3.drawString("G2 Pronto", this.getWidth() / 2+130, this.getHeight() - 300);
+            if (!g2.pronto) {
+                offgc3.drawString("Premere m per G2 Pronto", this.getWidth() / 2 + 130, this.getHeight() - 300);
+            } else {
+                offgc3.drawString("G2 Pronto", this.getWidth() / 2 + 130, this.getHeight() - 300);
             }
             g.drawImage(offscreen3, 0, 0, null);
         } else {
@@ -102,6 +102,39 @@ public class JFrame extends javax.swing.JFrame {
             }
 
             offgc.setColor(Color.white);
+            offgc.fillOval(p.x, p.y, 15, 15);
+
+            offgc.setColor(Color.white);
+            Font f1 = new Font("Arial", Font.PLAIN, 30);
+            offgc.setFont(f1);
+            offgc.drawString(g1.PunteggioToString(), this.getWidth() / 2 - 75, this.getHeight() - 600);
+            offgc.drawString(g2.PunteggioToString(), this.getWidth() / 2 + 75, this.getHeight() - 600);
+
+            g.drawImage(offscreen, 0, 0, null);
+
+            if (c.Finito()) {
+                Image offscreen2 = createImage(this.getWidth(), this.getHeight());
+                Graphics offgc2 = offscreen2.getGraphics();
+
+                offgc2.setColor(Color.black);
+                offgc2.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+                offgc2.setColor(Color.white);
+
+                Font f2 = new Font("Arial", Font.PLAIN, 50);
+                offgc2.setFont(f2);
+                offgc2.drawString("GAME OVER", this.getWidth() / 2 - 150, this.getHeight() / 2);
+
+                Font f3 = new Font("Arial", Font.PLAIN, 35);
+                offgc2.setFont(f3);
+                if (g1.getPunteggio() > g2.getPunteggio()) {
+                    offgc2.drawString("Player1 ha vinto", this.getWidth() / 2 - 115, this.getHeight() / 2 + 50);
+                } else {
+                    offgc2.drawString("Player2 ha vinto", this.getWidth() / 2 - 115, this.getHeight() / 2 + 50);
+                }
+
+                g.drawImage(offscreen2, 0, 0, null);
+            }
         }
     }
 
